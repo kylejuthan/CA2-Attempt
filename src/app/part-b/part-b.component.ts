9e7685378b623a9457784fc988034ab49fad209b
Enda Lee <enda.lee@ittdublin.ie>
2016-12-08 18:07:03 +0000

    Disaster 5.0

--- src/app/part-b/part-b.component.ts
+++ src/app/part-b/part-b.component.ts
@@ -16,4 +16,32 @@ export class PartBComponent{
       this.router.navigate(["part-a"]);        
     }
 
+    stone : number = 0;
+    pounds : number = 0;
+    orKgs : number = 0;
+    feet : number = 0;
+    inches : number = 0;
+    orMeters : number =0;
+
+StoneToKgs() {
+    this.orKgs = Math.round(this.stone * 6.35029318) ;
+  }
+StoneAndPoundToKgs(){
+  this.orKgs= Math.round(this.stone * 6.35029318 + this.pounds * 0.45359237);
+}
+orKgsToStoneAndPound() {
+  this.stone = Math.round(this.orKgs *  2.2046226218/14);
+  this.pounds = Math.round(this.orKgs * 0.45359237/2.2046226218);
+   
+}
+FeetToMetres() {
+    this.feet = this.orMeters * .3048;
+  }
+feetAndInchesToMeters() {
+    this.orMeters = this.feet * .3048 + this.inches * .0254
+  }
+inchesToMeters() {
+  this.inches = this.orMeters * .0254
+}
+
 }
